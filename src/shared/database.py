e71b555840136814bac35675c223f0ad7c0646fc
Thiet Quang Vo <tqv1@sfu.ca>
2025-11-21 19:38:11 -0800

    Update database.py, models.py, and docker-compose.yml

--- src/shared/database.py
+++ src/shared/database.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-
 import os
 from dotenv import load_dotenv
-
 engine = None
 def get_db():
     global engine
@@ -11,13 +9,12 @@ def get_db():
         host = os.environ['DATABASE_HOST']
         username = os.environ['DATABASE_USER']
         password = os.environ['DATABASE_PASSWORD']
-        
+
         DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@{host}:5432/"
-        
+
         engine = create_engine(DATABASE_URL)
 
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
     db = SessionLocal()
     try:
         yield db
